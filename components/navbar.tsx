b31eed54c092eaaeb52c660be45e742b5ef3b735
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-02-18 14:08:02 -0500

    feat: added dynamic logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
 	Navbar as NextUINavbar,
 	NavbarContent,
@@ -9,7 +11,7 @@ import {
 } from "@nextui-org/navbar";
 import { Link } from "@nextui-org/link";
 
-
+import { useTheme } from "next-themes";
 import { link as linkStyles } from "@nextui-org/theme";
 
 import { siteConfig } from "@/config/site";
@@ -23,16 +25,27 @@ import {
 	DiscordIcon,
 } from "@/components/icons";
 
+function Logo() {
+	const { theme } = useTheme();
+	return (
+		<div id="logo" className="h-[32px] w-[128px] relative font-bold">
+			<div id="prll" className={`h-[32px] w-[64px] logo-skew rounded-lg ${theme === "light" ? "bg-black" : "bg-white"}`}></div>
+        <span id="first-name" className={`absolute top-[3px] left-[5px] text-[20.5px] ${theme === "light" ? "text-white" : "text-black"}`}>carlos</span><span id="last-name absolute" className={`absolute top-[3px] left-[65px] text-[20.5px] ${theme === "light" ? "text-black" : "text-white"}`}>lespin</span>
+        </div>
+	)
+}
+
 
 export const Navbar = () => {
 	return (
 		<NextUINavbar maxWidth="xl" position="sticky">
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
-					<NextLink className="flex justify-start items-center gap-1" href="/">
+				<NextLink className="flex justify-start items-center gap-1" href="/">
 						
-						<p className="font-bold text-inherit">Carlos Lespin</p>
+						<Logo />
 					</NextLink>
+					
 				</NavbarBrand>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
 					{siteConfig.navItems.map((item) => (
