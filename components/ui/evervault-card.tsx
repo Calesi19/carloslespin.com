0cec9277dd160b49165fa4c657f3cb7c06279539
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-02-24 22:00:36 -0500

    Update evervault-card.tsx

--- components/ui/evervault-card.tsx
+++ components/ui/evervault-card.tsx
@@ -48,7 +48,7 @@ export const EvervaultCard = ({
         />
         <div className="relative z-10 flex items-center justify-center">
           <div className="relative h-44 w-44  rounded-full flex items-center justify-center text-white font-bold text-4xl">
-            <div className="absolute w-full h-full bg-white/[0.8] dark:bg-black/[0.0] blur-sm rounded-full" />
+            <div className="absolute w-full h-full bg-white/[0.8] dark:bg-black/[0.2] blur-sm rounded-full" />
             <span className="dark:text-white text-black z-20">{text}</span>
           </div>
         </div>
