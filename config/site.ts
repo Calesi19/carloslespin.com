4feb8075f287ec56ab9824f96059d877d0081a52
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-04-11 19:39:48 -0400

    fix: typo

--- config/site.ts
+++ config/site.ts
@@ -154,7 +154,7 @@ export const Resume = {
     },
     {
       title: "Languages",
-      items: ["Python", "JavaScript", "C++", "Go", "Dart", "TypeScript, C#"],
+      items: ["Python", "JavaScript", "C++", "Go", "Dart", "TypeScript", "C#"],
     },
   ],
 };
