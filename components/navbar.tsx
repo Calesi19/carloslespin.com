6dc3f33ddbc6c70ab79855b086364bc46f561988
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-12-23 09:24:16 -0400

    fix: nav links show on md

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -58,7 +58,7 @@ export const Navbar = (): React.JSX.Element => {
             <Logo />
           </NextLink>
         </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+        <ul className="hidden md:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
               <NextLink
